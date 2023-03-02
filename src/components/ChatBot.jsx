4acdb16c707646f434e3dfe5324e58fe3ae88cc3
ahmedsgit <acc.ahmedsazzad@gmail.com>
2023-03-02 18:52:35 +0600

    initial

---
+++ src/components/ChatBot.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MessengerCustomerChat from 'react-messenger-customer-chat/lib/MessengerCustomerChat'
+
+const FbChat = () => {
+    return (
+        <MessengerCustomerChat
+            pageId="108394628859432"
+            appId="175469848562805"
+        />
+    )
+}
+
+export default FbChat
